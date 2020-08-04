99f2c909436c8eb680be7118e7bc6129b7d82840
Max Owens <42765628+MOwneZ@users.noreply.github.com>
2020-08-05 11:50:55 +1200

    Update file_reader.py

--- model/file_reader.py
+++ model/file_reader.py
@@ -3,10 +3,16 @@ class FileReader:
     def __init__(self, file_dir):
         self.file_dir = file_dir
         self.file_contents = ""
+        self.clean_file = ""
 
+    # protected read file method
     def __read_file(self):
-        jsfile = open(self.file_dir)
-        self.file_contents = jsfile.readlines()
+        js_file = open(self.file_dir)
+        self.file_contents = js_file.readlines()
+        for line in self.file_contents:
+            self.clean_file += line
 
+    # public get file contents, as well as runs other function
     def get_file_contents(self):
-        return self.file_contents
+        self.__read_file()
+        return self.clean_file
