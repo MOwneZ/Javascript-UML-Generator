cfca93a53400d0c4ba192b50b553724e20ba9d8d
MOwneZ <42765628+MOwneZ@users.noreply.github.com>
2020-08-10 23:07:44 +1200

    update

--- controller/controller.py
+++ controller/controller.py
@@ -1,7 +1,9 @@
 from model.file_reader import FileReader
 from model.file_selector import FileSelect
+from model.js_uml_gen import JavaScriptReader
 
 TheFileSelector = FileSelect()
 dir = TheFileSelector.get_dir()
 TheFileReader = FileReader(dir)
-print(TheFileReader.get_file_contents())
+TheJSReader = JavaScriptReader(TheFileReader.get_file_contents())
+print(TheJSReader.get_raw())
